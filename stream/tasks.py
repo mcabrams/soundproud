c9867d740007c38b9df600337879ab1eff09c2a5
Matthew Abrams <mcabrams1@gmail.com>
2017-07-23 00:38:04 -0700

    Create task for persisting latest tracks

---
+++ stream/tasks.py
@@ -0,0 +1,8 @@
+from stream.gateways.soundcloud import SoundcloudGateway
+from track import models
+
+
+def persist_latest_stream_tracks():
+    gateway = SoundcloudGateway()
+    tracks = gateway.get_stream_tracks()
+    models.Track.objects.bulk_create(tracks)
---
+++ stream/tests/test_tasks.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from stream import tasks
+
+
+class PersistLatestStreamTracksTestCase(TestCase):
+    def setUp(self):
+        patcher = patch('stream.tasks.SoundcloudGateway')
+        self.SoundcloudGateway = patcher.start()
+        self.addCleanup(patcher.stop)
+
+    @patch('track.models.Track.objects')
+    def test_persists_stream_tracks(self, TrackManager):
+        tracks = self.SoundcloudGateway().get_stream_tracks()
+        tasks.persist_latest_stream_tracks()
+        TrackManager.bulk_create.assert_called_once_with(tracks)
