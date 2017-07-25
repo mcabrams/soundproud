bec557f56000042e308884eca16463e1810d48cc
Matthew Abrams <mcabrams1@gmail.com>
2017-07-24 18:25:28 -0700

    Migrate from fetch to axios; was getting weird acc. test failures presumably due to fetch not closing a connection?

--- assets/src/utils/api.js
+++ assets/src/utils/api.js
@@ -1,27 +1,5 @@
-let settings = {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-}
-
-function get(path) {
-  return fetch(path, settings)
-    .then(
-      function(res) {
-        if (res.status !== 200) {
-          console.log('Non-200; Status Code: ' + res.status)
-          return
-        }
-
-        return res.json()
-      })
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err)
-    });
-}
+import axios from 'axios'
 
 export function getTracks() {
-  return get('/tracks/')
+  return axios.get('/tracks/').then(tracks => tracks.data)
 }
