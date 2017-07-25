9d538f56a6749397207d320c35148d23c8f0bf79
Matthew Abrams <mcabrams1@gmail.com>
2017-07-24 21:55:21 -0700

    Simplify Tracklist class based view

--- stream/views.py
+++ stream/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from track.models import Track
 from track.serializers import TrackSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
@@ -9,9 +8,6 @@ def stream(request):
     return render(request, 'stream.html')
 
 
-class TrackList(mixins.ListModelMixin, generics.GenericAPIView):
+class TrackList(generics.ListAPIView):
     serializer_class = TrackSerializer
     queryset = Track.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
