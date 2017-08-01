64b4618d6cddff4a72bc5c3da1d75a589bd0f752
Matthew Abrams <mcabrams1@gmail.com>
2017-07-31 17:36:28 -0700

    Fix broken test

--- stream/tests/test_views.py
+++ stream/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class StreamTestCase(TestCase):
     def test_uses_correct_template(self):
-        res = self.client.get(self.url)
+        res = self.client.get(reverse('stream'))
         self.assertTemplateUsed(res, 'stream.html')
