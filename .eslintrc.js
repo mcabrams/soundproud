d1e796ebb59e7fc7068c9a83dd51bc7c409eb994
Matthew Abrams <mcabrams1@gmail.com>
2017-08-05 18:02:36 -0700

    Enforce closing bracket positioning in jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     semi: ['error', 'never'],
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
     'jsx-a11y/media-has-caption': 0,
+    'react/jsx-closing-bracket-location': 1,
   },
   plugins: [
     'jest',
