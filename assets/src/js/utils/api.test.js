213f0fc77f7fb8dc8d42471785ebeb4bc0061264
Matthew Abrams <mcabrams1@gmail.com>
2017-08-11 23:00:54 -0700

    Switch tests to use nock; fix up packages

--- assets/src/js/utils/api.test.js
+++ assets/src/js/utils/api.test.js
@@ -1,73 +1,69 @@
 import axios from 'axios'
+// $FlowFixMe
+import httpAdapter from 'axios/lib/adapters/http'
+import nock from 'nock'
 import * as api from './api'
+
+const host = 'http://localhost'
+axios.defaults.baseURL = host
+axios.defaults.adapter = httpAdapter
 
 describe('archiveTrack', () => {
   const track = { id: 42 }
 
-  beforeEach(() => {
-    jest.spyOn(axios, 'patch').mockImplementation(jest.fn())
-  })
+  it('should send patch request to correct uri', () => {
+    const archiveCall = nock(host)
+      .patch('/tracks/42/')
+      .reply(200, 'foo')
 
-  it('should call axios.patch correctly', () => {
-    api.archiveTrack(track)
-
-    expect(axios.patch).toHaveBeenCalledWith('/tracks/42/', {
-      archived: true,
+    return api.archiveTrack(track).then(() => {
+      expect(archiveCall.isDone()).toBeTruthy()
     })
-  })
-
-  it('should return axios.patch correctly', () => {
-    // $FlowFixMe
-    expect(api.archiveTrack(track)).toEqual(axios.patch())
   })
 })
 
 describe('getTracks', () => {
-  function getResponseObject(count) {
+  function getResponseObject(results = [], count = 1) {
     return {
-      data: {
-        results: [],
-        count,
-      },
+      results,
+      count,
     }
   }
 
-  beforeEach(() => {
-    const getMock = jest.fn(() => Promise.resolve(getResponseObject(13)))
-    jest.spyOn(axios, 'get').mockImplementation(getMock)
+  it('should get expected tracks', () => {
+    nock(host)
+      .get('/tracks/?page=1&archived=false')
+      .reply(200, getResponseObject())
+
+    return api.getTracks().then((response) => {
+      expect(response.tracks).toEqual([])
+    })
   })
 
-  it('should call axios.get with default page if omitted', () => {
-    api.getTracks()
+  it('should get specific page if passed', () => {
+    const call = nock(host)
+      .get('/tracks/?page=5&archived=false')
+      .reply(200, getResponseObject())
 
-    expect(axios.get).toHaveBeenCalledWith('/tracks/?page=1&archived=false')
-  })
-
-  it('should call axios.get with page if provided', () => {
-    api.getTracks(5)
-
-    expect(axios.get).toHaveBeenCalledWith('/tracks/?page=5&archived=false')
+    return api.getTracks(5).then(() => {
+      expect(call.isDone()).toBeTruthy()
+    })
   })
 
   describe('should yield correct pages left', () => {
     const countsAndExpecteds = [[0, 0], [1, 0], [10, 0], [11, 1], [21, 2]]
     const testCountYieldsExpected = ([count, expected]) => {
       it(`should have ${expected} pages left when count of ${count}`, () => {
-        const getMock = jest.fn(() => Promise.resolve(getResponseObject(count)))
+        const call = nock(host)
+          .get(/tracks/)
+          .reply(200, getResponseObject([], count))
 
-        jest.spyOn(axios, 'get').mockImplementation(getMock)
-
-        return api.getTracks(1).then((tracks) => {
-          expect(tracks.pagesLeft).toEqual(expected)
+        return api.getTracks(1).then((response) => {
+          expect(response.pagesLeft).toEqual(expected)
         })
       })
     }
 
     countsAndExpecteds.forEach(testCountYieldsExpected)
-  })
-
-  it('should return axios.get', () => {
-    // $FlowFixMe
-    expect(api.getTracks()).toEqual(axios.get())
   })
 })
