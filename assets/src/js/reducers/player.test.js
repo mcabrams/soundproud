804f34a94dfafab630b0c5ffa9802b6eb4363f48
Matthew Abrams <mcabrams1@gmail.com>
2017-08-13 21:34:55 -0700

    Add helper methods for determining adjacent tracks

--- assets/src/js/reducers/player.test.js
+++ assets/src/js/reducers/player.test.js
@@ -1,4 +1,5 @@
 import reducer from './player'
+import { nextTrackId, previousTrackId } from './player'
 import { trackFactory } from '../test/factories'
 
 const initialState = {
@@ -46,3 +47,48 @@ describe('player reducer', () => {
   })
 })
 
+describe('next track id selector', () => {
+  it('should select next track in allIds', () => {
+    const activeTrackId = 1
+    const allIds = [1, 2]
+
+    expect(nextTrackId(activeTrackId, allIds)).toEqual(2)
+  })
+
+  it('should select first track in allIds if in last position', () => {
+    const activeTrackId = 2
+    const allIds = [1, 2]
+
+    expect(nextTrackId(activeTrackId, allIds)).toEqual(1)
+  })
+
+  it('should return null if no tracks', () => {
+    const activeTrackId = null
+    const allIds = []
+
+    expect(nextTrackId(activeTrackId, allIds)).toEqual(null)
+  })
+})
+
+describe('previous track id selector', () => {
+  it('should select previous track in allIds', () => {
+    const activeTrackId = 2
+    const allIds = [1, 2]
+
+    expect(previousTrackId(activeTrackId, allIds)).toEqual(1)
+  })
+
+  it('should select last track in allIds if in first position', () => {
+    const activeTrackId = 1
+    const allIds = [1, 2]
+
+    expect(previousTrackId(activeTrackId, allIds)).toEqual(2)
+  })
+
+  it('should return null if no tracks', () => {
+    const activeTrackId = null
+    const allIds = []
+
+    expect(previousTrackId(activeTrackId, allIds)).toEqual(null)
+  })
+})
