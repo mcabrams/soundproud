5a9472d3818b064fddd7e3bb10bddf083042499e
Matthew Abrams <mcabrams1@gmail.com>
2017-09-16 00:09:08 -0700

    Redirect home to stream

--- web/stream/tests/test_views.py
+++ web/stream/tests/test_views.py
@@ -6,3 +6,9 @@ class StreamTestCase(TestCase):
     def test_uses_correct_template(self):
         res = self.client.get(reverse('stream'))
         self.assertTemplateUsed(res, 'stream.html')
+
+
+class HomeTestCase(TestCase):
+    def test_redirects_to_stream(self):
+        res = self.client.get('/')
+        self.assertRedirects(res, reverse('stream'))
--- web/stream/urls.py
+++ web/stream/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    url(r'^$',
+        RedirectView.as_view(url='stream/', permanent=False),
+        name='stream'),
     url(r'^stream/', views.stream, name='stream'),
     url(r'^tracks/$', views.TrackList.as_view()),
 ]
